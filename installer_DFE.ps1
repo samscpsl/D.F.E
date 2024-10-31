f5ba8977a2baf41d1f1ea68f7039a6be57460423
Sam <mrsamm282@gmail.com>
2024-10-31 21:46:45 +0300

    Add files via upload

--- installer_DFE.ps1
+++ installer_DFE.ps1
@@ -6,14 +6,16 @@ function DeleteLeftoverFiles
 	
 	# Delete existing installation folder
 	if (Test-Path $old_folder_path) {
-		Write-Output "РЈРґР°Р»РµРЅРёРµ СѓСЃС‚Р°СЂРµРІС€РёС… С„Р°Р№Р»РѕРІ"
+		Write-Output "Удаление устаревших файлов Winws"
 		[void](Remove-Item $old_folder_path -Recurse -Confirm:$False -Force)
 	}
 }
 
 # Set title and advertisement
 $host.ui.RawUI.WindowTitle = "D.F.E"
-Write-Host "Discord For Every one" -ForegroundColor yellow
+Write-Host "Загрузчик и инсталлятор сервиса Winws " -ForegroundColor white -nonewline
+Write-Host "282" -ForegroundColor yellow
+Write-Host "РУССКИЙ ЛЕС" -ForegroundColor yellow -BackgroundColor darkred
 Write-Host ""
 
 # Disable warnings and errors output
@@ -27,20 +29,16 @@ Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
 [void]([System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms"))
 
 # Determining Windows architecture
-$os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match вЂ(x64)вЂ™
+$os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
 
-$result = [System.Windows.Forms.MessageBox]::Show('Р’РѕРїСЂРѕСЃ:' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Р’С‹ РѕС‚РєР»СЋС‡РёР»Рё VPN, GoodbyeDPI, С‚СѓРЅРЅРµР»Рё Рё РїСЂРѕС‡РёРµ РїСЂРёР±Р»СѓРґС‹?' , "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
-	if ($result -eq 'No') 
-	{
-		exit
-	}
+$result = [System.Windows.Forms.MessageBox]::Show('Внимание!' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Выключите VPN/туннель/прочие приблуды перед установкой скрипта", "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 
 # Checking if Winws is already installed. Ask to uninstall. If not accepted, exit script.
-Write-Output "РџСЂРѕРІРµСЂРєР° РЅР°Р»РёС‡РёСЏ GoodbyeDPI"
+Write-Output "Проверка наличия GoodbyeDPI"
 $goodbyedpi = Get-Process goodbyedpi
 
 if ($goodbyedpi) {
-	$result = [System.Windows.Forms.MessageBox]::Show('РќР°Р№РґРµРЅ СЂР°Р±РѕС‚Р°СЋС‰РёР№ GoodbyeDPI.' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Р’С‹РєР»СЋС‡РёС‚Рµ GoodbyeDPI. РЎРµР№С‡Р°СЃ РѕРЅ Р±СѓРґРµС‚ РїСЂРёРЅСѓРґРёС‚РµР»СЊРЅРѕ РІС‹РєР»СЋС‡РµРЅ.", "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+	$result = [System.Windows.Forms.MessageBox]::Show('Найден работающий GoodbyeDPI.' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Выключите GoodbyeDPI, так как он конфликтует с Winws. Сейчас он будет принудительно выключен.", "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
 	[void]($goodbyedpi | Stop-Process -Force)
 }
 
@@ -56,15 +54,15 @@ else
 }
 
 # Checking if Winws is already installed. Ask to uninstall. If not accepted, exit script.
-Write-Output "РџСЂРѕРІРµСЂРєР° РЅР°Р»РёС‡РёСЏ СѓСЃС‚Р°РЅРѕРІР»РµРЅРЅРѕРіРѕ DFE"
+Write-Output "Проверка наличия установленного DFE"
 
 $winws_service_exists = Get-Service -Name "DFE" -ErrorAction SilentlyContinue
 
 if ($winws_service_exists.Length -gt 0) {
-	$result = [System.Windows.Forms.MessageBox]::Show('РќР°Р№РґРµРЅ СѓСЃС‚Р°РЅРѕРІР»РµРЅРЅС‹Р№ СЃРµСЂРІРёСЃ DFE' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'РЈРґР°Р»РёС‚СЊ?' , "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Error)
+	$result = [System.Windows.Forms.MessageBox]::Show('Найден установленный ранее сервис DFE' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Удалить?' , "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Error)
 	if ($result -eq 'Yes') {
 		# Deleting existing Winws service
-		Write-Output "РЈРґР°Р»РµРЅРёРµ DFE"
+		Write-Output "Удаление DFE"
 		[void](sc.exe stop "DFE")
 		[void](sc.exe delete "DFE")
 		DeleteLeftoverFiles -old_folder_path "$path"
@@ -75,7 +73,7 @@ if ($winws_service_exists.Length -gt 0) {
 	}
 }
 
-$result = [System.Windows.Forms.MessageBox]::Show('РЎРєСЂРёРїС‚ СѓСЃС‚Р°РЅРѕРІРёС‚ DFE РґР»СЏ РІРѕСЃСЃС‚Р°РЅРѕРІР»РµРЅРёСЏ РґРѕСЃС‚СѓРїР° Рє Discord Рё Youtube' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Р Р°Р·СЂРµС€РёС‚СЊ СѓСЃС‚Р°РЅРѕРІРєСѓ?' , "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
+$result = [System.Windows.Forms.MessageBox]::Show('Скрипт установит DFE для восстановления доступа к Discord и Youtube' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Разрешить установку?' , "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
 if ($result -eq 'Yes') {
 
 	$path = Split-Path $path -Parent
@@ -85,14 +83,14 @@ if ($result -eq 'Yes') {
 	[void](New-Item -Path "$path\$winws_folder" -ItemType Directory -Confirm:$False -Force)
 
 	# Downloading latest Winws to installtion folder
-	Write-Output "Р—Р°РіСЂСѓР·РєР° РїРѕСЃР»РµРґРЅРµР№ РІРµСЂСЃРёРё Winws"
+	Write-Output "Загрузка последней версии Winws"
 	
 	Invoke-WebRequest -Uri "https://github.com/bol-van/zapret-win-bundle/archive/refs/heads/master.zip" -OutFile "$path\zapret-win-bundle-master.zip"
 	
 	$winws_archive_name = "zapret-win-bundle-master.zip"
 
 	# Unpack downloaded archive
-	Write-Output "Р Р°СЃРїР°РєРѕРІРєР° Р°СЂС…РёРІР°"
+	Write-Output "Распаковка архива"
 	
 	Expand-Archive -Path "$path\$winws_archive_name" -DestinationPath $path
 	$unpacked_folder = "$path\$winws_archive_name".TrimEnd('.zip')
@@ -103,12 +101,12 @@ if ($result -eq 'Yes') {
 	if (Test-Path "$path\$winws_archive_name") {[void](Remove-Item "$path\$winws_archive_name" -Confirm:$False -Force)}
 	
 	# Download SCP:SL and Discord domains list File
-	Write-Output "Р—Р°РіСЂСѓР·РєР° sosalka_list.txt"
+	Write-Output "Загрузка sosalka_list.txt"
 		
 	Start-BitsTransfer -Source 'https://raw.githubusercontent.com/Sam282SD/D.F.E/refs/heads/main/sosalka_list.txt' -Destination "$path\$winws_folder"
 		
 	# Install Service
-	Write-Output "РЈСЃС‚Р°РЅРѕРІРєР° DFE..."
+	Write-Output "Установка DFE..."
 	
 	if ($os_type -eq $True) {
 		$exe_path = [Environment]::GetEnvironmentVariable("ProgramFiles") + "\" + $winws_folder + "\winws.exe"
@@ -118,13 +116,13 @@ if ($result -eq 'Yes') {
 		$exe_path = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)") + "\" + $winws_folder + "\winws.exe"
 	}
 	
-	$binaryPathName = "$exe_path --wf-tcp=80,443 --wf-udp=443,50000-50100 --filter-udp=443 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=`"$path\$winws_folder\quic_initial_www_google_com.bin`" --new --filter-udp=50000-50100 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic=`"$path\$winws_folder\quic_initial_www_google_com.bin`" --new --filter-tcp=80 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new --filter-tcp=443 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$path\$winws_folder\tls_clienthello_www_google_com.bin`""
-	[void](New-Service -Name "DFE" -BinaryPathName $binaryPathName -StartupType Automatic -Description "РћР±С…РѕРґ DPI by Sam")
+	$binaryPathName = "$exe_path --wf-tcp=80,443 --wf-udp=443,50000-65535 --filter-udp=443 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=`"$path\$winws_folder\quic_initial_www_google_com.bin`" --new --filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic=`"$path\$winws_folder\quic_initial_www_google_com.bin`" --new --filter-tcp=80 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new --filter-tcp=443 --hostlist=`"$path\$winws_folder\sosalka_list.txt`" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$path\$winws_folder\tls_clienthello_www_google_com.bin`""
+	[void](New-Service -Name "DFE" -BinaryPathName $binaryPathName -StartupType Automatic -Description "Обход DPI by Sam")
 	
-	Write-Output "Р—Р°РїСѓСЃРє DFE"
+	Write-Output "Запуск DFE"
 	[void](Start-Service -Name "DFE")
 	
-	$result = [System.Windows.Forms.MessageBox]::Show('DFE СѓСЃРїРµС€РЅРѕ СѓСЃС‚Р°РЅРѕРІР»РµРЅ' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Р§С‚РѕР±С‹ СѓРґР°Р»РёС‚СЊ СЃРєСЂРёРїС‚, Р·Р°РїСѓСЃС‚РёС‚Рµ СЃРЅРѕРІР° РґР°РЅРЅС‹Р№ С„Р°Р№Р», РіРґРµ РІР°Рј РїСЂРµРґР»РѕР¶Р°С‚ СѓРґР°Р»РёС‚СЊ D.F.E", "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+	$result = [System.Windows.Forms.MessageBox]::Show('DFE успешно установлен' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Чтобы удалить скрипт, запустите снова данный файл, где вам предложат удалить D.F.E", "D.F.E" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 }
 if ($result -eq 'No') {
 	exit
